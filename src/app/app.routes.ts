0f0ba3d88abfeec3738f7979704e7e7e9164bf48
buihieu05 <buihieu12345678905@gmail.com>
2026-07-01 20:00:45 +0700

    lab2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { About } from "./about/about";
+import { Contact } from './pages/contact/contact';
+export const routes: Routes = [
+    {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+];
